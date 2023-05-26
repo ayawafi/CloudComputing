f1bf9c55543f43df4db733ec1018d90c5656e3f3
Aya Wafi <ayawafi.2534@gmail.com>
2023-05-26 19:03:50 +0300

    Add project files.

---
+++ TestJWT/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestJWT.Models;
+using TestJWT.Services;
+
+namespace TestJWT.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthServices _authServices;
+
+        public AuthController(IAuthServices authServices)
+        {
+            _authServices = authServices;
+        }
+
+        
+    }
+}
---
+++ TestJWT/Services/AuthServices.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+using TestJWT.Helper;
+using TestJWT.Models;
+using System.Linq;
+using Microsoft.Extensions.Options;
+
+namespace TestJWT.Services
+{
+    public class AuthServices
+    {
+      
+    }
+}
+
